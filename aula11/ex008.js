233b2ca8bbd42c806fcb57c8f697c20d67f0ef50
Daniel Pereira Vasconcelos nunes <daniel.pvn@hotmail.com>
2021-11-23 19:46:18 -0300

    Initial commit

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+//condição composta
+
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('voce ultrapassou. multado')
+}
+console.log('dirija com cuidado')
